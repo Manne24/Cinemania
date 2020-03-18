7a389976da72f185bd5bbd1a08018ee1e5e0c409
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-03-18 21:29:37 +0100

    step one, youtube trailer plus lots of npm(maybe not necessary)

--- src/main/resources/static/components/filmList.js
+++ src/main/resources/static/components/filmList.js
@@ -8,6 +8,10 @@ export default {
     description: {{ film.description}} <br>
     length: {{ film.length}} <br>
     {{ film.trailer }}
+    <iframe id="ytplayer" type="text/html" width="640" height="360"
+  src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe>
+    <!-- <iframe width='640' height='390' src="https://www.youtube.com/watch?v=gCcx85zbxz4"></iframe> -->
     <hr>
     </li>
    </ul>
