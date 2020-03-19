1b12892f7774bbc8b8df958b5a07c1e4a2b1304d
Chirnil <chiharu.kazama@gmail.com>
2020-03-19 17:30:52 +0100

    controller,repo,service for user

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,4 +1,31 @@
 package com.example.demo.controllers;
 
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUser(){
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/users/{user_id}")
+    public User getOneUser(@PathVariable int user_id){
+        return userService.findOneUser(user_id);
+    }
+
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,4 +1,25 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepo userRepo;
+
+    public List<User> findAllUsers(){
+        return (List<User>) userRepo.findAll();
+    }
+
+    public User findOneUser(int user_id){
+        User user = userRepo.findById(user_id);
+        if(user==null) return null;
+
+        return user;
+    }
 }
