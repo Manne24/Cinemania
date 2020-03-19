7064ed61869aee61cb07380bd0c94891d5993f46
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-03-19 09:38:14 +0100

    entity film added

--- src/main/java/com/example/demo/entities/Film.java
+++ src/main/java/com/example/demo/entities/Film.java
@@ -1,4 +1,42 @@
 package com.example.demo.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "films")
+
 public class Film {
+
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment
+    private int id;
+    private String title;
+    private String description;
+
+    public Film() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
