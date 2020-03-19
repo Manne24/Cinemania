f4331654de9cf7af928317226499895ed8f52a45
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-03-19 11:29:45 +0100

    Testing youtube

--- src/main/resources/static/views/films.js
+++ src/main/resources/static/views/films.js
@@ -10,6 +10,9 @@ export default {
         title: {{ film.title }} <br>
         director: {{ film.director }} <br>
         description: {{ film.description}} <br>
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+        src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+        frameborder="0"></iframe>
         <hr>
         </li>
     </ul>
