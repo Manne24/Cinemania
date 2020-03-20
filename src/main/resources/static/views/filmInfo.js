61b85f7c3ac59c9135a79924feafc7e2cafb0ae9
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-03-20 21:21:18 +0100

    Click a film and it will go to filmInfo :-)

--- src/main/resources/static/views/filmInfo.js
+++ src/main/resources/static/views/filmInfo.js
@@ -1,18 +1,31 @@
 export default{
     template:`
      <div>
-     <h1>Films</h1>
- <!-- <iframe id="ytplayer" type="text/html" width="640" height="360"
-        src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
-        frameborder="0"></iframe> -->
-   <!--  <h1>Films</h1>
+     <h1>{{film.title}}</h1>
+<iframe width="640" height="360"
+        src="https://www.youtube.com/watch?v=film.trailer"
+        frameborder="0"></iframe>
+    <section>
         <img :src="film.image" alt="film image"><br>
         title: {{ film.title }} <br>
         director: {{ film.director }} <br>
         description: {{ film.description}} <br>
-        <button @click="showTrailer">Trailer</button> -->
+        <button>Trailer</button>
+        </section>
    </div>
     `,
+    data(){
+        return {
+            film: {
+                title: '',
+                image: '',
+                director: '',
+                description: '',
+                trailer: ''
+            }
+            
+        }
+    },
     async created() {
 
         let film = await fetch('/rest/films/' + this.$route.params.id)
@@ -20,6 +33,7 @@ export default{
         
         console.log(film)
          
+        this.film = film
     }
 
 }
--- src/main/resources/static/views/films.js
+++ src/main/resources/static/views/films.js
@@ -6,27 +6,26 @@ export default {
         src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
         frameborder="0"></iframe> -->
     <h1>Films</h1>
-        <ul>
-        <li v-for="film of films" :key="film.id">
+    <div class="filmcard" 
+         v-for="film of films" 
+         :key="film.id"
+         @click="goToFilmInfo(film.id)">
         <img :src="film.image" alt="film image"><br>
         title: {{ film.title }} <br>
         director: {{ film.director }} <br>
         description: {{ film.description}} <br>
         <!-- <button @click="showTrailer">Trailer</button> -->
-        <hr>
-        </li>
-    </ul>
+    </div>
    </div>
     `,
+    methods: {
+        goToFilmInfo(id){
+            this.$router.push('/films/' + id)
+        }
+    }, 
     computed: {
         films() {
             return this.$store.state.films
         }
-    }/* ,
-    methods: {
-        showTrailer(){
-            let trailer = 
-            console.log('film.trailer')
-        }
-    } */
+    }
 }
