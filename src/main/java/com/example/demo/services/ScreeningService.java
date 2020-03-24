daef408cadb382515606998350d9c51a8ca4ab01
Emmanuel <Emmanuel_ndziba@hotmail.se>
2020-03-24 11:39:33 +0100

    Repo and Service

---
+++ src/main/java/com/example/demo/services/ScreeningService.java
@@ -0,0 +1,25 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Screening;
+import com.example.demo.repositories.ScreeningRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ScreeningService {
+
+    @Autowired
+    ScreeningRepo screeningRepo;
+
+   public List<Screening> findAllScreenings() {
+       return (List<Screening>) screeningRepo.findAll();
+   }
+
+   public Optional<Screening> getOneScreening(int id) {
+       return screeningRepo.findById(id);
+   }
+
+}
