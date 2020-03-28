986ff7041e8a1d0d0b2a5f64f62f27f3a94c2742
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-03-28 22:24:17 +0100

    added error message

--- src/main/resources/static/components/addNewFilm.js
+++ src/main/resources/static/components/addNewFilm.js
@@ -5,8 +5,9 @@ export default {
         <label>ADD FILM</label><br>
         <input v-model="titleAdd" type="text" placeholder="Enter title of film"><br>
         <button type="submit">ADD</button>
-        </form><br><br>
-
+        </form>
+        <p style="color:red">{{ filmNotFound }}</p><br><br><br>
+        
         <form @submit.prevent="deleteFilm">
         <label>REMOVE FILM</label><br>
         <input v-model="titleDelete" type="text" placeholder="Enter title of film"><br>
@@ -27,56 +28,64 @@ export default {
             image: '',
             genre: '',
             rated: '',
-            runtime: ''
+            runtime: '',
+            filmNotFound: 'Film not found on OMDB-API'
         }
     },
     computed: {
         films() {
             return this.$store.state.films
         }
-    },
-    async addNewFilm() {
+    }, methods: {
+        checkIfFilmExists() {
+            /* if(imdbInfo.length > 0) {
+                addNewFilm
+            } else {
+                return filmNotFound
+            } */
+        },
+        async addNewFilm() {
+            if (!this.title.trim() &&
+                !this.description.trim() &&
+                !this.director.trim() &&
+                !this.image.trim() &&
+                !this.genre.trim() &&
+                !this.rated.trim() &&
+                !this.runtime.trim()) {
+                return
+            }
 
-        if (!this.title.trim() &&
-            !this.description.trim() &&
-            !this.director.trim() &&
-            !this.image.trim() &&
-            !this.genre.trim() &&
-            !this.rated.trim() &&
-            !this.runtime.trim()) {
-            return
+            let filmInfoAPI = {
+                title: this.imdbInfo.Title,
+                director: this.imdbInfo.Director,
+                description: this.imdbInfo.Plot,
+                image: this.imdbInfo.Poster,
+                genre: this.imdbInfo.Genre,
+                rated: this.imdbInfo.Rated,
+                runtime: this.imdbInfo.Runtime
+            }
+
+            let result = await fetch('/rest/films', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(filmInfoAPI)
+            })
+
+            result = await result.json()
+            console.log(result)
+
+            this.title = '',
+                this.director = '',
+                this.description = '',
+                this.director = '',
+                this.image = '',
+                this.genre = '',
+                this.rated = '',
+                this.runtime = ''
+
         }
-
-        let filmInfoAPI = {
-            title: this.imdbInfo.Title,
-            director: this.imdbInfo.Director,
-            description: this.imdbInfo.Plot,
-            image: this.imdbInfo.Poster,
-            genre: this.imdbInfo.Genre,
-            rated: this.imdbInfo.Rated,
-            runtime: this.imdbInfo.Runtime
-        }
-
-        let result = await fetch('/rest/films', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(filmInfoAPI)
-        })
-
-        result = await result.json()
-        console.log(result)
-
-        this.title = '',
-            this.director = '',
-            this.description = '',
-            this.director = '',
-            this.image = '',
-            this.genre = '',
-            this.rated = '',
-            this.runtime = ''
-
     },
     async created() {
 
