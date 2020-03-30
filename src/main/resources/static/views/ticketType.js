37b7a92d1ac47e02ec07c3ea363e82027e79883a
Emmanuel Ndziba <Emmanuel_Ndziba@hotmaill.se>
2020-03-30 14:03:39 +0200

    working on tickettype page, mostly css

--- src/main/resources/static/views/ticketType.js
+++ src/main/resources/static/views/ticketType.js
@@ -1,28 +1,49 @@
 export default {
   template: `
-    <div>
-        <h4>Select Ticket Amount</h4>
-        <form>
+    <div class="ticketBox">
+        <h4 class="ticketTitle">Select Ticket Amount</h4>
+        <form class="ticketForm">
+          <div class="inputBox">
             <p>Child</p>
-            <div class="value-button" id="decrease"  value="Decrease Value">+</div>
-                <input type="number" id="number" value="0" />
-            <div class="value-button" id="increase" value="Increase Value">-</div>
-            <p>Adult</p>
-            <div class="value-button" id="decrease"  value="Decrease Value">+</div>
-                <input type="number" id="number" value="0" />
-            <div class="value-button" id="increase" value="Increase Value">-</div>
-            <p>Senior</p>
-            <div class="value-button" id="decrease"  value="Decrease Value">+</div>
-                <input type="number" id="number" value="0" />
-            <div class="value-button" id="increase" value="Increase Value">-</div>
+              <div class="value-button" id="decrease" @click="decreaseValue">-</div>
+                <input type="number" v-model="value" id="number" value="0" />
+              <div class="value-button" id="increase" @click="increaseValue">+</div>
+            </div>
+            <div class="inputBox">
+              <p>Adult</p>
+                <div class="value-button" id="decrease" @click="decreaseValue">-</div>
+                  <input type="number" v-model="value" id="number" value="0" />
+                <div class="value-button" id="increase" @click="increaseValue">+</div>
+              </div>
+            <div class="inputBox">
+              <p>Senior</p>
+                <div class="value-button" id="decrease" @click="decreaseValue">-</div>
+                  <input type="number" v-model="value" id="number" value="0" />
+              <div class="value-button" id="increase" @click="increaseValue">+</div>
+            </div>           
         </form>
-        <button @click="goToSeats">Continue</button>
+        <button @click="goToSeats" class="goToSeatsButton">Choose Seats</button>
     </div>
     
     `,
+    data() {
+     return  {
+       value:0
+  
+     }
+    },
   methods: {
     goToSeats() {
       this.$router.push("/seats");
+    },
+    decreaseValue() {
+      console.log("minus");
+      this.value -= 1;
+    },
+
+    increaseValue() {
+      console.log("plus");
+      this.value += 1;
     }
   }
 };
