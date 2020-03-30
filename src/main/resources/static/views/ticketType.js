022ccf7c12a60eecf4eee0de6b525db8c779def9
alx-b <alexandre.blain@hyhs.se>
2020-03-30 15:25:21 +0200

    fixing tickets selection.

--- src/main/resources/static/views/ticketType.js
+++ src/main/resources/static/views/ticketType.js
@@ -5,45 +5,58 @@ export default {
         <form class="ticketForm">
           <div class="inputBox">
             <p>Child</p>
-              <div class="value-button" id="decrease" @click="decreaseValue">-</div>
+              <div class="value-button" id="decrease" @click="decreaseValue(1)">-</div>
                 <input type="number" v-model="value" id="number" value="0" />
-              <div class="value-button" id="increase" @click="increaseValue">+</div>
+              <div class="value-button" id="increase" @click="increaseValue(1)">+</div>
             </div>
             <div class="inputBox">
               <p>Adult</p>
-                <div class="value-button" id="decrease" @click="decreaseValue">-</div>
-                  <input type="number" v-model="value" id="number" value="0" />
-                <div class="value-button" id="increase" @click="increaseValue">+</div>
+                <div class="value-button" id="decrease" @click="decreaseValue(2)">-</div>
+                  <input type="number" v-model="value2" id="number" value="0" />
+                <div class="value-button" id="increase" @click="increaseValue(2)">+</div>
               </div>
             <div class="inputBox">
               <p>Senior</p>
-                <div class="value-button" id="decrease" @click="decreaseValue">-</div>
-                  <input type="number" v-model="value" id="number" value="0" />
-              <div class="value-button" id="increase" @click="increaseValue">+</div>
+                <div class="value-button" id="decrease" @click="decreaseValue(3)">-</div>
+                  <input type="number" v-model="value3" id="number" value="0" />
+              <div class="value-button" id="increase" @click="increaseValue(3)">+</div>
             </div>           
         </form>
         <button @click="goToSeats" class="goToSeatsButton">Choose Seats</button>
     </div>
     
     `,
-    data() {
-     return  {
-       value:0
-  
-     }
-    },
+  data() {
+    return {
+      value: 0,
+      value2: 0,
+      value3: 0
+    };
+  },
   methods: {
     goToSeats() {
       this.$router.push("/seats");
     },
-    decreaseValue() {
+    decreaseValue(selectedValue) {
       console.log("minus");
-      this.value -= 1;
+      if (selectedValue === 1 && this.value > 0) {
+        this.value -= 1;
+      } else if (selectedValue === 2 && this.value2 > 0) {
+        this.value2 -= 1;
+      } else if (selectedValue === 3 && this.value3 > 0) {
+        this.value3 -= 1;
+      }
     },
 
-    increaseValue() {
+    increaseValue(selectedValue) {
       console.log("plus");
-      this.value += 1;
+      if (selectedValue === 1) {
+        this.value += 1;
+      } else if (selectedValue === 2) {
+        this.value2 += 1;
+      } else if (selectedValue === 3) {
+        this.value3 += 1;
+      }
     }
   }
 };
