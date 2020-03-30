b99fab24820b6622d64881d124062fc1651cbe11
Emmanuel Ndziba <Emmanuel_Ndziba@hotmaill.se>
2020-03-30 16:42:04 +0200

    fix small css n js

--- src/main/resources/static/views/ticketType.js
+++ src/main/resources/static/views/ticketType.js
@@ -4,23 +4,24 @@ export default {
         <h4 class="ticketTitle">Select Ticket Amount</h4>
         <form class="ticketForm">
           <div class="inputBox">
-            <p>Child</p>
+            <p>Child <span>65kr each</span> </p>
               <div class="value-button" id="decrease" @click="decreaseValue(1)">-</div>
                 <input type="number" v-model="value" id="number" value="0" />
               <div class="value-button" id="increase" @click="increaseValue(1)">+</div>
             </div>
             <div class="inputBox">
-              <p>Adult</p>
+              <p>Adult <span>85kr each</span> </p>
                 <div class="value-button" id="decrease" @click="decreaseValue(2)">-</div>
                   <input type="number" v-model="value2" id="number" value="0" />
                 <div class="value-button" id="increase" @click="increaseValue(2)">+</div>
               </div>
             <div class="inputBox">
-              <p>Senior</p>
+              <p>Senior <span>75kr each</span> </p>
                 <div class="value-button" id="decrease" @click="decreaseValue(3)">-</div>
                   <input type="number" v-model="value3" id="number" value="0" />
               <div class="value-button" id="increase" @click="increaseValue(3)">+</div>
             </div>           
+            <p class="priceText">Total Price: {{price}} kr </p>
         </form>
         <button @click="goToSeats" class="goToSeatsButton">Choose Seats</button>
     </div>
@@ -30,7 +31,8 @@ export default {
     return {
       value: 0,
       value2: 0,
-      value3: 0
+      value3: 0,
+      price:0
     };
   },
   methods: {
@@ -38,24 +40,22 @@ export default {
       this.$router.push("/seats");
     },
     decreaseValue(selectedValue) {
-      console.log("minus");
       if (selectedValue === 1 && this.value > 0) {
-        this.value -= 1;
+        this.value -= 1, this.price -= 65;
       } else if (selectedValue === 2 && this.value2 > 0) {
-        this.value2 -= 1;
+        this.value2 -= 1, this.price -= 85;
       } else if (selectedValue === 3 && this.value3 > 0) {
-        this.value3 -= 1;
+        this.value3 -= 1, this.price -= 75;
       }
     },
 
     increaseValue(selectedValue) {
-      console.log("plus");
       if (selectedValue === 1) {
-        this.value += 1;
+        this.value += 1, this.price += 65;
       } else if (selectedValue === 2) {
-        this.value2 += 1;
+        this.value2 += 1 , this.price += 85;
       } else if (selectedValue === 3) {
-        this.value3 += 1;
+        this.value3 += 1 , this.price += 75;
       }
     }
   }
