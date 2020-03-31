14cf4d85e07aeb6543717869fb3c87a387d19168
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-03-31 16:10:50 +0200

    Logout works, but still some functionality to improve

--- src/main/resources/static/views/Cinemania.js
+++ src/main/resources/static/views/Cinemania.js
@@ -2,6 +2,7 @@ export default {
     template:`
     <div>
         <h2 class="title">Cinemania</h2>
+        <iframe src="https://giphy.com/embed/9gISqB3tncMmY" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/cat-star-wars-9gISqB3tncMmY">via GIPHY</a></p>
     </div>
     `
 }
--- src/main/resources/static/views/login.js
+++ src/main/resources/static/views/login.js
@@ -1,5 +1,5 @@
 export default {
-    template: `
+  template: `
       <form class="login" @submit.prevent="performLogin">
         <!-- <h2>Login</h2> -->
         <label>Email :</label><br>
@@ -13,41 +13,42 @@ export default {
         <button @click="goToSignUp">Click here</button>
       </form>
     `,
-    data() {
-      return {
-        email: '',
-        password: ''
-      }
-    },
-    methods: {
-      async performLogin() {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async performLogin() {
 
-        const credentials = 'email=' +
+      const credentials = 'email=' +
         encodeURIComponent(this.email)
         + '&password=' +
         encodeURIComponent(this.password)
 
-        let response = await fetch("/login", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: credentials
-        });
+      let response = await fetch("/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: credentials
+      });
 
-        if(response.url.includes('error')) {
-          console.log('Wrong email/password');
-        }
-        else {
-          this.$router.push('/mypage');
-          
-          let user = await fetch('/auth/whoami')
-          user = await user.json()
-          console.log('Successfully logged in:', user)
-        }
-        },
-
-      goToSignUp(){
-        location.href ="/register";
-
+      if (response.url.includes('error')) {
+        console.log('Wrong email/password');
       }
+      else {
+        this.$router.push('/mypage');
+
+        let user = await fetch('/auth/whoami')
+        user = await user.json()
+        this.$store.commit('setUser', user)
+        console.log('Successfully logged in:', user)
+      }
+    },
+
+    goToSignUp() {
+      location.href = "/register";
+
     }
   }
+}
