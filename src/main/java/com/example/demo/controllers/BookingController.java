f32a3449476d9ddcc93f0fae39f65d30052b2c99
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-04-02 09:24:24 +0200

    BookingController

--- src/main/java/com/example/demo/controllers/BookingController.java
+++ src/main/java/com/example/demo/controllers/BookingController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Booking;
 import com.example.demo.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest")
@@ -11,5 +17,15 @@ public class BookingController {
 
     @Autowired
     BookingService bookingService;
+
+    @GetMapping("/bookings")
+    public List<Booking> getAllBookings(){
+        return bookingService.findAllBookings();
+    }
+
+    @GetMapping("/bookings/{booking_id}")
+    public Optional<Booking> getOneBooking(@PathVariable int booking_id){
+        return bookingService.findOneBooking(booking_id);
+    }
 
 }
