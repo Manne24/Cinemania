7e7da633ac38399035cf0437a3dc69dcb27c1434
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-04-02 22:42:57 +0200

    booking delete in progress

--- src/main/resources/static/views/myPage.js
+++ src/main/resources/static/views/myPage.js
@@ -51,17 +51,20 @@ export default {
             headers: { "Content-Type": "application/json" },
             body: this.user_id
           });
-    
-              
-            //   let user = await fetch('/auth/whoami')
+                //   let user = await fetch('/auth/whoami')
             //   user = await user.json()
               this.$store.state.users
               console.log('Successfully resigned your membership')
             //   this.$store.commit('setUser', user)
             //   console.log('Successfully logged in:', user)
-    
             //   this.$router.push('/mypage');
             }
         },
+        async cancelBooking() {
+            let response = await fetch("/bookings/"+ this.user_id, {
+              method: "DELETE",
+              headers: { "Content-Type": "application/json" },
+              body: this.user_id
+            });
 }
 
