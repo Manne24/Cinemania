bf472d456998f7d102701575eb0e4fb98376f916
Chirnil <chiharu.kazama@gmail.com>
2020-04-03 21:53:42 +0200

    ta bort saker

--- src/main/resources/static/views/myPage.js
+++ src/main/resources/static/views/myPage.js
@@ -11,12 +11,9 @@ export default {
         <div class="user-info">
             User_id: {{ user.user_id }}<br>
             Name: {{ user.name }}<br>
-            Email: {{ user.email }}
+            Email: {{ user.email }}<br>
         </div>
 
-        <button @click="quitUser">Cancel membership</button>
-        <label>Enter your user id :</label><br>
-        <input v-model="user_id" required type="id" placeholder="user_id..."><br>
         <h2>My booking history</h2>
         <history/>
     </div>    
@@ -26,21 +23,5 @@ export default {
         user(){
             return this.$store.state.user
         }
-    },
-    methods: {
-        async quitUser() {
-          let response = await fetch("/users/"+ this.user_id, {
-            method: "DELETE",
-            headers: { "Content-Type": "application/json" },
-            body: this.user_id
-          });
-                //   let user = await fetch('/auth/whoami')
-            //   user = await user.json()
-              this.$store.state.users
-              console.log('Successfully resigned your membership')
-            //   this.$store.commit('setUser', user)
-            //   console.log('Successfully logged in:', user)
-            //   this.$router.push('/mypage');
-            }
-        },
+    }
     }
