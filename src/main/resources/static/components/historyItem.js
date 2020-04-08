6ce80db9e98cd1b91058e8de63928bf9319d4d0a
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-04-08 12:12:31 +0200

    End tag (</div>) missing

--- src/main/resources/static/components/historyItem.js
+++ src/main/resources/static/components/historyItem.js
@@ -10,6 +10,7 @@ export default {
         Booking id: {{ booking.booking_id }} <br>
         Booking time: {{ booking.booking_time }} <br> 
         <i class="far fa-trash-alt" @click="cancelBooking"></i>
+        </div>
         <div>
         <historyItemScreening
             class="ticket-card" 
